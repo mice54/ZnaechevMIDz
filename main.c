b1227abfa69b2f4e2e007fae0cf0e8c88006e1dc
mice54 <145797713+mice54@users.noreply.github.com>
2024-11-19 08:01:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <Graphics.h>
+void paintTree(int x,int y){
+line(x,y,x+50,y+50);
+line(x,y,x-50,y+50);
+line(x-50,y+50,x+50,y+50);
+y+=50;
+line(x,y,x+50,y+50);
+line(x,y,x-50,y+50);
+line(x-50,y+50,x+50,y+50);
+y+=50;
+line(x-10,y,x-10,y+25);
+line(x+10,y,x+10,y+25);
+line(x+10,y+25,x-10,y+25);
+}
 void paintDom(int x,int y){
-
 line(x,y,x+50,y+50);
 line(x,y,x-50,y+50);
 line(x-50,y+50,x+50,y+50);
@@ -12,9 +24,13 @@ line(x-50,y+75,x+50,y+75);
 int main(){
 int gd=DETECT,gm;
 initgraph(&gd,&gm,"C:\\BGI");
+paintTree(100,100);
+getch();
+closegraph();
+initgraph(&gd,&gm,"C:\\BGI");
 paintDom(100,100);
 getch();
 closegraph();
 return 0;
 }
-
+
